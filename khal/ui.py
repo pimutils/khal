3d5fc5db4cc0d62066cb914ba378cc08501bf795
Christian Geier <geier@lostpackets.de>
2013-08-12 16:59:07 +0200

    ikhal now shows a day's events on the right

--- khal/ui.py
+++ khal/ui.py
@@ -1,6 +1,8 @@
 import urwid
 import calendar
 from datetime import date
+from datetime import time
+from datetime import datetime
 
 palette = [('header', 'white', 'black'),
            ('reveal focus', 'black', 'dark cyan', 'standout'),
@@ -42,7 +44,37 @@ def week_list(count=3):
     return khal
 
 
-def construct_week(week):
+class DateColumns(urwid.Columns):
+    def __init__(self, widget_list, call=None, **kwargs):
+        self.call = call
+
+        super(DateColumns, self).__init__(widget_list, **kwargs)
+
+    def _set_focus_position(self, position):
+        """
+        Set the widget in focus.
+
+        position -- index of child widget to be made focus
+        """
+
+        try:
+            if position < 0 or position >= len(self.contents):
+                raise IndexError
+        except (TypeError, IndexError):
+            raise IndexError, "No Columns child widget at position %s" % (position,)
+        # since first Column is month name, focus should only be 0 during
+        # construction
+        if not self.contents.focus == 0:
+            self.call(self.contents[position][0].original_widget.date)
+        self.contents.focus = position
+
+    focus_position = property(urwid.Columns._get_focus_position, _set_focus_position, doc="""
+index of child widget in focus. Raises IndexError if read when
+Columns is empty, or when set to an invalid index.
+""")
+
+
+def construct_week(week, call=None):
     """
     :param week: list of datetime.date objects
     returns urwid.Columns
@@ -56,25 +88,65 @@ def construct_week(week):
 
     for day in week:
         if day == date.today():
-            this_week.append((2, urwid.AttrMap(Date(day), 'today', 'today_focus')))
+            this_week.append((2, urwid.AttrMap(Date(day),
+                                               'today', 'today_focus')))
         else:
-            this_week.append((2, urwid.AttrMap(Date(day), None, 'reveal focus')))
-    #this_week = this_week + [(2, urwid.AttrMap(Date(day), None, 'reveal focus')) for day in week]
-    week = urwid.Columns(this_week, dividechars=1)
+            this_week.append((2, urwid.AttrMap(Date(day),
+                                               None, 'reveal focus')))
+    week = DateColumns(this_week, call=call, dividechars=1)
     return week
 
 
-def interactive(conf=None, dbtool=None):
+def calendar_walker(call=None):
+    """hopefully this will soon become a real "walker",
+    loading new weeks as nedded"""
     lines = list()
     daynames = 'Mo Tu We Th Fr Sa Su'.split(' ')
     daynames = urwid.Columns([(4, urwid.Text('    '))] + [(2, urwid.Text(name)) for name in daynames],
                              dividechars=1)
     lines = [daynames]
     for week in week_list():
-        week = construct_week(week)
+        week = construct_week(week, call=call)
         lines.append(week)
 
     weeks = urwid.Pile(lines)
+    return weeks
 
-    fill = urwid.Filler(weeks)
+
+class EventList(urwid.WidgetWrap):
+
+    def __init__(self, conf=None, dbtool=None):
+        self.conf = conf
+        self.dbtool = dbtool
+        self.number = 1
+        pile = urwid.Pile([])
+        urwid.WidgetWrap.__init__(self, pile)
+        self.update()
+
+    def update(self, this_date=date.today()):
+
+        self.number += 1
+        start = datetime.combine(this_date, time.min)
+        end = datetime.combine(this_date, time.max)
+
+        event_column = [this_date.strftime('%d.%m.%Y')]
+        all_day_events = list()
+        events = list()
+        for account in self.conf.sync.accounts:
+            all_day_events += self.dbtool.get_allday_range(this_date,
+                                                            account_name=account)
+            events += self.dbtool.get_time_range(start, end, account)
+        for event in all_day_events:
+            event_column.append(event.summary)
+        for event in events:
+            event_column.append(event.start.strftime('%H:%M') + '-' +  event.end.strftime('%H:%M') + ': ' + event.summary)
+        pile = urwid.Pile([urwid.Text(event) for event in event_column])
+        self._w = pile
+
+
+def interactive(conf=None, dbtool=None):
+    events = EventList(conf=conf, dbtool=dbtool)
+    weeks = calendar_walker(call=events.update)
+    columns = urwid.Columns([(25, weeks), events], dividechars=2)
+    fill = urwid.Filler(columns)
     urwid.MainLoop(fill, palette=palette).run()
