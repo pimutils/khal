255bd0c03af3126c1cd0f98989077ed14c54fb2c
Christian Geier <geier@lostpackets.de>
2014-03-25 14:30:04 +0100

    fix: don't log deleting events twice

--- khal/backend.py
+++ khal/backend.py
@@ -393,7 +393,6 @@ class SQLiteDb(object):
         returns nothing
         """
         self._check_account(account)
-        logging.debug("locally deleting " + str(href))
         for dbname in [account + '_d', account + '_dt', account + '_m']:
             sql_s = 'DELETE FROM {0} WHERE href = ? ;'.format(dbname)
             self.sql_ex(sql_s, (href, ))
