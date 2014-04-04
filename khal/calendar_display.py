870691f07ad3c55a73d319fea519f33536d51288
Christian Geier <geier@lostpackets.de>
2014-04-04 18:51:58 +0200

    cleanup: calendar_display_test (also: tests!)
    will soon be integrated into a new common module for all terminal
    interface related stuff

--- khal/calendar_display.py
+++ khal/calendar_display.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # coding: utf-8
 # vim: set ts=4 sw=4 expandtab sts=4:
 # Copyright (c) 2011-2014 Christian Geier & contributors
@@ -23,19 +22,14 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 from __future__ import print_function
 
-import argparse
 import calendar
 import datetime
-import doctest
 
 from khal.aux import bstring, rstring
 
 
 def getweeknumber(date):
     """return iso week number for datetime.date object
-    >>> getweeknumber(datetime.date(2012,12,12))
-    50
-
     :param date: date
     :type date: datetime.date()
     :return: weeknumber
@@ -48,13 +42,12 @@ def str_week(week, today):
     """returns a string representing one week,
     if for day == today colour is reversed
 
-    :param week: list of 6 datetime.date objects (one week)
+    :param week: list of 7 datetime.date objects (one week)
     :type day: list()
     :param today: the date of today
     :type today: datetime.date
     :return: string, which if printed on terminal appears to have length 20,
              but may contain ascii escape sequences
-
     :rtype: str
     """
     strweek = ''
@@ -65,29 +58,6 @@ def str_week(week, today):
             day = str(day.day).rjust(2)
         strweek = strweek + day + ' '
     return strweek
-
-
-def print_month(month=datetime.date.today().month,
-                year=datetime.date.today().year,
-                today=datetime.date.today()):
-    """returns a single month calendar, current date highlighted,
-       much like cal(1) or python calendar.prmonth
-    """
-    khal = ''
-    mycal = calendar.Calendar(0)  # 0: week starts on monday
-    month_name = calendar.month_name[month] + ' ' + str(year)
-    khal = month_name.center(20) + '\n'
-    khal = khal + bstring('Mo Tu We Th Fr Sa Su') + '\n'
-    for mday, wday in mycal.itermonthdays2(year, month):
-        if mday == 0:
-            mday = ''
-        elif mday == today.day and month == today.month and year == today.year:
-            mday = rstring(str(mday).rjust(2))
-        khal = khal + str(mday).rjust(2) + ' '
-        if wday % 7 == 6:
-            khal = khal + '\n'
-    khal = khal + '\n'
-    return khal
 
 
 def vertical_month(month=datetime.date.today().month,
@@ -110,25 +80,8 @@ def vertical_month(month=datetime.date.today().month,
     :returns: calendar strings,  may also include some
               ANSI (color) escape strings
     :rtype: list() of str()
-
-    >>> vertical_month(month=12, year=2011, today=datetime.date(2011,12,12))
-    ['\\x1b[1m    Mo Tu We Th Fr Sa Su\\x1b[0m ',
-     '\\x1b[1mDec \\x1b[0m28 29 30  1  2  3  4 ',
-     '     5  6  7  8  9 10 11 ',
-     '    \\x1b[7m12\\x1b[0m 13 14 15 16 17 18 ',
-     '    19 20 21 22 23 24 25 ',
-     '\\x1b[1mJan \\x1b[0m26 27 28 29 30 31  1 ',
-     '     2  3  4  5  6  7  8 ',
-     '     9 10 11 12 13 14 15 ',
-     '    16 17 18 19 20 21 22 ',
-     '    23 24 25 26 27 28 29 ',
-     '\\x1b[1mFeb \\x1b[0m30 31  1  2  3  4  5 ',
-     '     6  7  8  9 10 11 12 ',
-     '    13 14 15 16 17 18 19 ',
-     '    20 21 22 23 24 25 26 ',
-     '\\x1b[1mMar \\x1b[0m27 28 29  1  2  3  4 ']
-
     """
+
     khal = list()
     w_number = '    ' if weeknumber else ''
     calendar.setfirstweekday(firstweekday)
@@ -151,28 +104,3 @@ def vertical_month(month=datetime.date.today().month,
             month = 1
             year = year + 1
     return khal
-
-
-def config_parser():
-    carg_parser = argparse.ArgumentParser(
-        description='displaying calendar like cal(1)')
-    carg_parser.add_argument('-v', '--version', action='version',
-                             version='0.1')
-    carg_parser.add_argument('-w', action='store_true', dest='no_week',
-                             default=False, help='do NOT display weeknumber')
-    carg_parser.add_argument('-m', action='store', dest='month_number',
-                             default=3, type=int,
-                             help='number of months to print, default: 3')
-    args = vars(carg_parser.parse_args())
-    return args
-
-# print(print_month())
-#print(print_month(month=12, year=2011, today=datetime.date(2011, 12, 1)))
-#print(print_month(month=7, year=2012))
-#print(vertical_month(month=12, year=2011))
-
-if __name__ == '__main__':
-    doctest.testmod()
-    args = config_parser()
-    print('\n'.join(vertical_month(weeknumber=(not args['no_week']),
-                                   count=args['month_number'])))
---
+++ tests/cal_display_test.py
@@ -0,0 +1,104 @@
+import datetime
+
+import pytest
+
+from khal.calendar_display import vertical_month, getweeknumber, str_week
+
+
+today = datetime.date.today()
+yesterday = today - datetime.timedelta(days=1)
+tomorrow = today + datetime.timedelta(days=1)
+
+
+def test_getweeknumber():
+    assert getweeknumber(datetime.date(2011,12,12)) == 50
+    assert getweeknumber(datetime.date(2011,12,31)) == 52
+    assert getweeknumber(datetime.date(2012,1,1)) == 52
+    assert getweeknumber(datetime.date(2012,1,2)) == 1
+
+
+def test_str_week():
+    aday = datetime.date(2012, 6, 1)
+    bday = datetime.date(2012, 6, 8)
+    week = [datetime.date(2012, 6, 6),
+            datetime.date(2012, 6, 7),
+            datetime.date(2012, 6, 8),
+            datetime.date(2012, 6, 9),
+            datetime.date(2012, 6, 10),
+            datetime.date(2012, 6, 11),
+            datetime.date(2012, 6, 12),
+            datetime.date(2012, 6, 13)]
+    assert str_week(week, aday) == ' 6  7  8  9 10 11 12 13 '
+    assert str_week(week, bday) == ' 6  7 \x1b[7m 8\x1b[0m  9 10 11 12 13 '
+
+
+
+example1 =  [
+    '\x1b[1m    Mo Tu We Th Fr Sa Su \x1b[0m',
+    '\x1b[1mDec \x1b[0m28 29 30  1  2  3  4 ',
+    '     5  6  7  8  9 10 11 ',
+    '    \x1b[7m12\x1b[0m 13 14 15 16 17 18 ',
+    '    19 20 21 22 23 24 25 ',
+    '\x1b[1mJan \x1b[0m26 27 28 29 30 31  1 ',
+    '     2  3  4  5  6  7  8 ',
+    '     9 10 11 12 13 14 15 ',
+    '    16 17 18 19 20 21 22 ',
+    '    23 24 25 26 27 28 29 ',
+    '\x1b[1mFeb \x1b[0m30 31  1  2  3  4  5 ',
+    '     6  7  8  9 10 11 12 ',
+    '    13 14 15 16 17 18 19 ',
+    '    20 21 22 23 24 25 26 ',
+    '\x1b[1mMar \x1b[0m27 28 29  1  2  3  4 ']
+
+example_weno = [
+    '\x1b[1m    Mo Tu We Th Fr Sa Su     \x1b[0m',
+    '\x1b[1mDec \x1b[0m28 29 30  1  2  3  4 \x1b[1m 48\x1b[0m',
+    '     5  6  7  8  9 10 11 \x1b[1m 49\x1b[0m',
+    '    \x1b[7m12\x1b[0m 13 14 15 16 17 18 \x1b[1m 50\x1b[0m',
+    '    19 20 21 22 23 24 25 \x1b[1m 51\x1b[0m',
+    '\x1b[1mJan \x1b[0m26 27 28 29 30 31  1 \x1b[1m 52\x1b[0m',
+    '     2  3  4  5  6  7  8 \x1b[1m 1\x1b[0m',
+    '     9 10 11 12 13 14 15 \x1b[1m 2\x1b[0m',
+    '    16 17 18 19 20 21 22 \x1b[1m 3\x1b[0m',
+    '    23 24 25 26 27 28 29 \x1b[1m 4\x1b[0m',
+    '\x1b[1mFeb \x1b[0m30 31  1  2  3  4  5 \x1b[1m 5\x1b[0m',
+    '     6  7  8  9 10 11 12 \x1b[1m 6\x1b[0m',
+    '    13 14 15 16 17 18 19 \x1b[1m 7\x1b[0m',
+    '    20 21 22 23 24 25 26 \x1b[1m 8\x1b[0m',
+    '\x1b[1mMar \x1b[0m27 28 29  1  2  3  4 \x1b[1m 9\x1b[0m']
+
+example_we_start_su = [
+    '\x1b[1m    Su Mo Tu We Th Fr Sa \x1b[0m',
+    '\x1b[1mDec \x1b[0m27 28 29 30  1  2  3 ',
+    '     4  5  6  7  8  9 10 ',
+    '    11 \x1b[7m12\x1b[0m 13 14 15 16 17 ',
+    '    18 19 20 21 22 23 24 ',
+    '    25 26 27 28 29 30 31 ',
+    '\x1b[1mJan \x1b[0m 1  2  3  4  5  6  7 ',
+    '     8  9 10 11 12 13 14 ',
+    '    15 16 17 18 19 20 21 ',
+    '    22 23 24 25 26 27 28 ',
+    '\x1b[1mFeb \x1b[0m29 30 31  1  2  3  4 ',
+    '     5  6  7  8  9 10 11 ',
+    '    12 13 14 15 16 17 18 ',
+    '    19 20 21 22 23 24 25 ',
+    '\x1b[1mMar \x1b[0m26 27 28 29  1  2  3 ']
+
+
+
+def test_vertical_month():
+    vert_str = vertical_month(month=12, year=2011,
+                              today=datetime.date(2011, 12, 12))
+    assert vert_str == example1
+
+    weno_str = vertical_month(month=12, year=2011,
+                              today=datetime.date(2011, 12, 12),
+                              weeknumber=True)
+    assert weno_str == example_weno
+
+
+    we_start_su_str = vertical_month(
+        month=12, year=2011,
+        today=datetime.date(2011, 12, 12),
+        firstweekday=6)
+    assert we_start_su_str == example_we_start_su
