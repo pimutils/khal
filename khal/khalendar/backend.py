da9908da0d7aa0a0872beb89a6820121d5d89198
Christian Geier <geier@lostpackets.de>
2014-06-04 01:04:34 +0200

    Octal literals are no longer of the form 0720; use 0o720 instead.

--- khal/khalendar/backend.py
+++ khal/khalendar/backend.py
@@ -130,7 +130,7 @@ class SQLiteDb(object):
         if not path.isdir(dbdir):
             try:
                 logger.debug('trying to create the directory for the db')
-                makedirs(dbdir, mode=0770)
+                makedirs(dbdir, mode=0o770)
                 logger.debug('success')
             except OSError as error:
                 logger.fatal('failed to create {0}: {1}'.format(dbdir, error))
