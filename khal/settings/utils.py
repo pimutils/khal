a92d27c95cb275230061e1282294ff275359edb8
Christian Geier <geier@lostpackets.de>
2014-08-26 21:52:01 +0200

    bugfix: default timezone setting was broken

--- khal/settings/utils.py
+++ khal/settings/utils.py
@@ -74,7 +74,9 @@ def config_checks(config):
         raise ValueError('Found no calendar in the config file')
     test_default_calendar(config)
     config['sqlite']['path'] = expand_db_path(config['sqlite']['path'])
-    config['locale']['default_timezone'] = is_timezone(
-        config['locale']['default_timezone'])
-    config['locale']['local_timezone'] = is_timezone(
-        config['locale']['local_timezone'])
+    if not config['locale']['default_timezone']:
+        config['locale']['default_timezone'] = is_timezone(
+            config['locale']['default_timezone'])
+    if not config['locale']['local_timezone']:
+        config['locale']['local_timezone'] = is_timezone(
+            config['locale']['local_timezone'])
