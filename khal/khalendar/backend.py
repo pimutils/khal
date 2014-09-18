ed13e98bfe1e36901ec0b9102638b015843e5c2c
Christian Geier <geier@lostpackets.de>
2014-09-18 21:34:15 +0200

    dead code removal

--- khal/khalendar/backend.py
+++ khal/khalendar/backend.py
@@ -218,21 +218,6 @@ class SQLiteDb(object):
         self.sql_ex(sql_s, stuple)
         logger.debug("made sure tables for {0} exists".format(self.calendar))
 
-    def needs_update(self, href_etag_list):
-        """checks if we need to update this vcard
-        :param href_etag_list: list of tuples of (hrefs and etags)
-        :return: list of hrefs that need an update
-        """
-        needs_update = list()
-        for href, etag in href_etag_list:
-            stuple = (href,)
-            sql_s = 'SELECT etag FROM {0} WHERE href = ?'.format(
-                self.calendar + '_m')
-            result = self.sql_ex(sql_s, stuple)
-            if not result or etag != result[0][0]:
-                needs_update.append(href)
-        return needs_update
-
     def update(self, vevent, href=None, etag='',
                ignore_invalid_items=False):
         """insert a new or update an existing card in the db
