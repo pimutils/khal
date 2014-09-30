6c54850c136964dc14a1b03b140d767b2ba563c4
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-09-30 10:36:45 +0200

    Use native strings for logging
    The fix for #114 doesn't work if the item contains nonascii characters.
    The ical variable seems to be a bytestring, and when formatting it into
    a unicode string, Python wants to decode it with the ascii codec. Doing
    so raises a UnicodeError (instead of UpdateFailed) and we still get an
    error message with normal logging settings.

--- khal/khalendar/backend.py
+++ khal/khalendar/backend.py
@@ -244,8 +244,8 @@ class SQLiteDb(object):
                     events.append(component)
 
         if len(events) == 0:
-            logger.debug(u'Could not find event in {}'.format(ical))
-            raise UpdateFailed(u'Could not find event in {}'.format(ical))
+            logger.debug('Could not find event in {}'.format(ical))
+            raise UpdateFailed('Could not find event in {}'.format(ical))
 
         elif len(events) > 1:
             if len(set([event['UID'] for event in events])):
