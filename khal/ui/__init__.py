3ad63d609851e92521b42f87b18365da387162b1
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-02-16 11:16:36 +0100

    This doesn't seem to do anything useful
    Also it was responsible for a glitch, when selecting days without
    events.

--- khal/ui/__init__.py
+++ khal/ui/__init__.py
@@ -190,7 +190,6 @@ class DateCColumns(CColumns):
         if key in self.keys['view']:
             self._old_attr_map = self.contents[self.focus_position][0].get_attr_map()
             self._old_pos = old_pos
-            self.contents[self.focus_position][0].set_attr_map({None: 'today focus'})
             return 'right'
         elif self._old_attr_map:
             self.contents[self._old_pos][0].set_attr_map(self._old_attr_map)
