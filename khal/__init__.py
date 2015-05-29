6309d2c8fc5306f084bd21938c1479e6ea65f50f
Christian Geier <geier@lostpackets.de>
2015-05-30 01:20:04 +0200

    relative import

--- khal/__init__.py
+++ khal/__init__.py
@@ -20,7 +20,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from khal import version
+from . import version
 
 __productname__ = 'khal'
 __version__ = version.__version__
