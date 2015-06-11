ad081971cb5956a889100f350cd44fd02d34fdeb
Christian Geier <geier@lostpackets.de>
2015-06-11 22:18:47 +0200

    fix at for new guessdatetime

--- khal/cli.py
+++ khal/cli.py
@@ -325,7 +325,7 @@ def _get_cli():
             dtime = datetime.datetime.now()
         else:
             try:
-                dtime = aux.guessdatetimefstr(dtime_list, locale)
+                dtime, _ = aux.guessdatetimefstr(dtime_list, locale)
             except ValueError:
                 logger.fatal(
                     '{} is not a valid datetime (matches neither {} nor {} nor'
