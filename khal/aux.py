8832c4e13fd4e02fba128a9087f5c16e0fcdff41
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-08-11 18:50:54 +0200

    Add extra assertion

--- khal/aux.py
+++ khal/aux.py
@@ -238,6 +238,7 @@ def construct_event(dtime_list, locale,
     see tests for examples
 
     """
+    assert all(isinstance(x, str) for x in dtime_list)
     today = datetime.today()
     try:
         dtstart, all_day = guessdatetimefstr(dtime_list, locale)
