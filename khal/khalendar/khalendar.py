93b84de541315b621df386bb4086253b3e989d8f
Dominik Joe Pantůček <joe@joe.cz>
2015-10-05 15:43:22 +0200

    Add background highlighting styles and use them - dark only.

--- khal/khalendar/khalendar.py
+++ khal/khalendar/khalendar.py
@@ -384,17 +384,20 @@ class CalendarCollection(object):
             self.get_allday_by_time_range(day)
         if len(devents) == 0:
             return None
+        prefix = ''
+        if self.hmethod == 'bg' or self.hmethod == 'background':
+            prefix = 'bg '
         if self.color != '':
-            return self.color
+            return prefix+self.color
         dcolors = list(set(map(lambda x: self.get_event_color(x), devents)))
         if len(dcolors) == 1:
             if devents[0].color == '':
-                return self.default_color
+                return prefix+self.default_color
             else:
-                return devents[0].color
+                return prefix+devents[0].color
         if self.multiple != '':
-            return self.multiple
-        return (dcolors[0], dcolors[1])
+            return prefix+self.multiple
+        return (prefix+dcolors[0], prefix+dcolors[1])
 
     def get_styles(self, date, focus):
         if focus:
