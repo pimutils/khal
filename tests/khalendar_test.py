92c44f974c7fc4b4e483dce0090f8174e0e63d07
Christian Geier <geier@lostpackets.de>
2016-01-08 01:33:07 +0100

    flake8

--- tests/khalendar_test.py
+++ tests/khalendar_test.py
@@ -70,20 +70,23 @@ class TestCalendar(object):
 
         print('init')
         for calendar in coll._calendars:
-            print('{}: saved ctag: {}, vdir ctag: {}'.format(calendar, coll._local_ctag(calendar), coll._backend.get_ctag(calendar)))
+            print('{}: saved ctag: {}, vdir ctag: {}'.format(
+                calendar, coll._local_ctag(calendar), coll._backend.get_ctag(calendar)))
         assert len(list(vdirs[cal1].list())) == 0
         assert coll._needs_update(cal1) is False
 
         vdirs[cal1].upload(item_today)
         print('upload')
         for calendar in coll._calendars:
-            print('{}: saved ctag: {}, vdir ctag: {}'.format(calendar, coll._local_ctag(calendar), coll._backend.get_ctag(calendar)))
+            print('{}: saved ctag: {}, vdir ctag: {}'.format(
+                calendar, coll._local_ctag(calendar), coll._backend.get_ctag(calendar)))
         assert len(list(vdirs[cal1].list())) == 1
         assert coll._needs_update(cal1) is True
         coll.update_db()
         print('updated')
         for calendar in coll._calendars:
-            print('{}: saved ctag: {}, vdir ctag: {}'.format(calendar, coll._local_ctag(calendar), coll._backend.get_ctag(calendar)))
+            print('{}: saved ctag: {}, vdir ctag: {}'.format(
+                calendar, coll._local_ctag(calendar), coll._backend.get_ctag(calendar)))
         assert coll._needs_update(cal1) is False
 
 
