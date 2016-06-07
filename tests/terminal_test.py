4e1c7116c5df0f7a1d7e84542249eff28a469f26
Christian Geier <geier@lostpackets.de>
2016-06-07 16:24:28 +0200

    flake8ing

--- tests/terminal_test.py
+++ tests/terminal_test.py
@@ -10,7 +10,8 @@ def test_colored():
     assert colored('täst', 'light magenta', 'light green', True) == '\x1b[1;35m\x1b[42mtäst\x1b[0m'
     assert colored('täst', 'light magenta', 'light green', False) == '\x1b[95m\x1b[102mtäst\x1b[0m'
     assert colored('täst', '5', '20') == '\x1b[38;5;5m\x1b[48;5;20mtäst\x1b[0m'
-    assert colored('täst', '#F0F', '#00AABB') == '\x1b[38;2;255;0;255m\x1b[48;2;0;170;187mtäst\x1b[0m'
+    assert colored('täst', '#F0F', '#00AABB') == \
+        '\x1b[38;2;255;0;255m\x1b[48;2;0;170;187mtäst\x1b[0m'
 
 
 class TestMergeColumns(object):
