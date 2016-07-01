5e795134c4250f19b6525616cd7b092de3fb1d03
Pierre DAVID <pdagog@gmail.com>
2016-07-01 09:15:44 +0200

    Style checking

--- tests/event_test.py
+++ tests/event_test.py
@@ -73,11 +73,13 @@ def test_update_simple():
     event.update_categories('meeting')
     assert normalize_component(event.raw) == normalize_component(event_updated.raw)
 
+
 def test_update_remove_categories():
     event = Event.fromString(_get_text('event_dt_simple_updated'), **EVENT_KWARGS)
     event_nocat = Event.fromString(_get_text('event_dt_simple_nocat'), **EVENT_KWARGS)
     event.update_categories('    ')
     assert normalize_component(event.raw) == normalize_component(event_nocat.raw)
+
 
 def test_raw_d():
     event_d = _get_text('event_d')
