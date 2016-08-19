a26cc93392868793ab53d953429c7301c697b271
Christian Geier <geier@lostpackets.de>
2016-08-19 21:38:59 +0200

    Fix checking if RDATE uses PERIOD
    fix #480

--- khal/khalendar/backend.py
+++ khal/khalendar/backend.py
@@ -634,7 +634,7 @@ def check_support(vevent, href, calendar):
             .format(href, calendar)
         )
     rdate = vevent.get('RDATE')
-    if rdate is not None and rdate.params.get('VALUE') == 'PERIOD':
+    if rdate is not None and hasattr(rdate, 'params') and rdate.params.get('VALUE') == 'PERIOD':
         raise UpdateFailed(
             '`RDATE;VALUE=PERIOD` is currently not supported by khal. '
             'Therefore event {0} from calendar {1} will not be shown in khal.\n'
