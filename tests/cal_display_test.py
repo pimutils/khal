72c8efc929bd245ed233cc80cfc6d696cc511831
Christian Geier <geier@lostpackets.de>
2016-08-21 20:10:21 +0200

    Fix German locale tests on *BSD

--- tests/cal_display_test.py
+++ tests/cal_display_test.py
@@ -137,6 +137,23 @@ example_de = [
     '    20 21 22 23 24 25 26 ',
     '\x1b[1mM\xe4r \x1b[0m27 28 29  1  2  3  4 ']
 
+example_de_bsd = [
+    '\x1b[1m    Mo Di Mi Do Fr Sa So \x1b[0m',
+    '\x1b[1mDez.\x1b[0m28 29 30  1  2  3  4 ',
+    '     5  6  7  8  9 10 11 ',
+    '    \x1b[7m12\x1b[0m 13 14 15 16 17 18 ',
+    '    19 20 21 22 23 24 25 ',
+    '\x1b[1mJan.\x1b[0m26 27 28 29 30 31  1 ',
+    '     2  3  4  5  6  7  8 ',
+    '     9 10 11 12 13 14 15 ',
+    '    16 17 18 19 20 21 22 ',
+    '    23 24 25 26 27 28 29 ',
+    '\x1b[1mFeb.\x1b[0m30 31  1  2  3  4  5 ',
+    '     6  7  8  9 10 11 12 ',
+    '    13 14 15 16 17 18 19 ',
+    '    20 21 22 23 24 25 26 ',
+    '\x1b[1mM\xe4rz\x1b[0m27 28 29  1  2  3  4 ']
+
 
 def test_vertical_month():
     try:
@@ -172,7 +189,9 @@ def test_vertical_month_unicode():
         locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
         vert_str = vertical_month(month=12, year=2011,
                                   today=datetime.date(2011, 12, 12))
-        assert vert_str == example_de
+        # de_DE locale on at least Net and FreeBSD is different from the one
+        # commonly used on linux systems
+        assert vert_str in [example_de, example_de_bsd]
         '\n'.join(vert_str)  # issue 142
     except locale.Error as error:
         if str(error) == 'unsupported locale setting':
