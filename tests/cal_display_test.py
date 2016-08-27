c1d50b789c177a6253a6ea59c56def308a89be52
Christian Geier <geier@lostpackets.de>
2016-08-28 00:11:12 +0200

    FreeBSD and NetBSD german locale *is* different

--- tests/cal_display_test.py
+++ tests/cal_display_test.py
@@ -137,7 +137,7 @@ example_de = [
     '    20 21 22 23 24 25 26 ',
     '\x1b[1mMär \x1b[0m27 28 29  1  2  3  4 ']
 
-example_de_bsd = [
+example_de_freebsd = [
     '\x1b[1m    Mo Di Mi Do Fr Sa So \x1b[0m',
     '\x1b[1mDez.\x1b[0m28 29 30  1  2  3  4 ',
     '     5  6  7  8  9 10 11 ',
@@ -153,6 +153,23 @@ example_de_bsd = [
     '    13 14 15 16 17 18 19 ',
     '    20 21 22 23 24 25 26 ',
     '\x1b[1mMärz\x1b[0m27 28 29  1  2  3  4 ']
+
+example_de_netbsd = [
+    '\x1b[1m    Mo Di Mi Do Fr Sa So \x1b[0m',
+    '\x1b[1mDez.\x1b[0m28 29 30  1  2  3  4 ',
+    '     5  6  7  8  9 10 11 ',
+    '    \x1b[7m12\x1b[0m 13 14 15 16 17 18 ',
+    '    19 20 21 22 23 24 25 ',
+    '\x1b[1mJan.\x1b[0m26 27 28 29 30 31  1 ',
+    '     2  3  4  5  6  7  8 ',
+    '     9 10 11 12 13 14 15 ',
+    '    16 17 18 19 20 21 22 ',
+    '    23 24 25 26 27 28 29 ',
+    '\x1b[1mFeb.\x1b[0m30 31  1  2  3  4  5 ',
+    '     6  7  8  9 10 11 12 ',
+    '    13 14 15 16 17 18 19 ',
+    '    20 21 22 23 24 25 26 ',
+    '\x1b[1mM\xe4r.\x1b[0m27 28 29  1  2  3  4 ']
 
 
 def test_vertical_month():
@@ -180,7 +197,7 @@ def test_vertical_month_unicode():
                                   today=datetime.date(2011, 12, 12))
         # de_DE locale on at least Net and FreeBSD is different from the one
         # commonly used on linux systems
-        assert vert_str in [example_de, example_de_bsd]
+        assert vert_str in [example_de, example_de_freebsd, example_de_netbsd]
         '\n'.join(vert_str)  # issue 142
     except locale.Error as error:
         if str(error) == 'unsupported locale setting':
