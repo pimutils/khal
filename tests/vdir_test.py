b49e89dd5228167abf57c41113decfb4e9472859
Christian Geier <geier@lostpackets.de>
2016-11-22 11:51:21 +0100

    first test for vdir.get_etag_from_file()

---
+++ tests/vdir_test.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2013-2016 Christian Geier et al.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import os
+
+from khal.khalendar import vdir
+
+
+def test_etag(tmpdir):
+    fpath = os.path.join(str(tmpdir), 'foo')
+
+    file_ = open(fpath, 'w')
+    file_.write('foo')
+    file_.close()
+
+    old_etag = vdir.get_etag_from_file(fpath)
+
+    file_ = open(fpath, 'w')
+    file_.write('foo')
+    file_.close()
+
+    new_etag = vdir.get_etag_from_file(fpath)
+
+    assert old_etag != new_etag
