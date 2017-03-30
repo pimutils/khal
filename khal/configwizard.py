e213a017a9eef1c8602af739256fa41cbe30a7c4
Thomas Kluyver <thomas@kluyver.me.uk>
2017-03-30 23:00:23 +0100

    Add some docstrings

--- khal/configwizard.py
+++ khal/configwizard.py
@@ -166,6 +166,12 @@ def get_vdirs_from_vdirsyncer_config():
 
 
 def find_vdir():
+    """Use one or more existing vdirs on the system.
+    
+    Tries to get data from vdirsyncer if it's installed and configured, and
+    asks user to confirm it. If not, prompt the user for the path to a single
+    vdir.
+    """
     print("The following collections were found:")
     synced_vdirs = get_vdirs_from_vdirsyncer_config()
     if synced_vdirs:
@@ -230,6 +236,10 @@ password = {password}
 
 
 def vdirsyncer_config_path():
+    """Find where vdirsyncer will look for it's config.
+    
+    There may or may not already be a file at the returned path.
+    """
     fname = environ.get('VDIRSYNCER_CONFIG', None)
     if fname is None:
         fname = normpath(expanduser('~/.vdirsyncer/config'))
@@ -291,6 +301,7 @@ def create_synced_vdir():
 
 
 def start_syncing():
+    """Run vdirsyncer to sync the newly created vdir with the remote."""
     print("Syncing calendar...")
     try:
         exit_code = call(['vdirsyncer', 'discover'])
@@ -309,6 +320,7 @@ def start_syncing():
 
 
 def choose_vdir_calendar():
+    """query the user for their preferred calendar source"""
     choices = [
         ("Create a new calendar on this computer", create_vdir),
         ("Use a calendar already on this computer (vdir format)", find_vdir),
