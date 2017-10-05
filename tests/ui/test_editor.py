5e2ae10308590a2e518796456bbdf3fe330e623e
Christian Geier <geier@lostpackets.de>
2017-10-05 22:44:43 +0200

    Tests for StartEndEditor

--- tests/ui/test_editor.py
+++ tests/ui/test_editor.py
@@ -4,11 +4,20 @@ import icalendar
 from khal.ui.editor import RecurrenceEditor, StartEndEditor
 
 from ..utils import BERLIN, LOCALE_BERLIN
+from .canvas_render import CanvasTranslator
 
 CONF = {'locale': LOCALE_BERLIN, 'keybindings': {}, 'view': {'monthdisplay': 'firstday'}}
 
 START = BERLIN.localize(dt.datetime(2015, 4, 26, 22, 23))
 END = BERLIN.localize(dt.datetime(2015, 4, 27, 23, 23))
+
+palette = {
+    'date header focused': 'blue',
+    'date header': 'green',
+    'default': 'black',
+    'editf': 'red',
+    'edit': 'blue',
+}
 
 
 def test_popup(monkeypatch):
@@ -65,3 +74,60 @@ def test_check_understood_rrule():
     assert not RecurrenceEditor.check_understood_rrule(
         icalendar.vRecur.from_ical('FREQ=MONTHLY;BYDAY=TH;BYSETPOS=3')
     )
+
+
+def test_editor():
+    """test for the issue in #666"""
+    editor = StartEndEditor(
+        BERLIN.localize(dt.datetime(2017, 10, 2, 13)),
+        BERLIN.localize(dt.datetime(2017, 10, 4, 18)),
+        conf=CONF
+    )
+    assert editor.startdt == BERLIN.localize(dt.datetime(2017, 10, 2, 13))
+    assert editor.enddt == BERLIN.localize(dt.datetime(2017, 10, 4, 18))
+    assert editor.changed is False
+    for _ in range(3):
+        editor.keypress((10, ), 'tab')
+    for _ in range(3):
+        editor.keypress((10, ), 'shift tab')
+    assert editor.startdt == BERLIN.localize(dt.datetime(2017, 10, 2, 13))
+    assert editor.enddt == BERLIN.localize(dt.datetime(2017, 10, 4, 18))
+    assert editor.changed is False
+
+
+def test_convert_to_date():
+    """test for the issue in #666"""
+    editor = StartEndEditor(
+        BERLIN.localize(dt.datetime(2017, 10, 2, 13)),
+        BERLIN.localize(dt.datetime(2017, 10, 4, 18)),
+        conf=CONF
+    )
+    canvas = editor.render((50, ), True)
+    assert CanvasTranslator(canvas, palette).transform() == (
+        '[ ] Allday\nFrom: \x1b[31m2.10.2017 \x1b[0m \x1b[34m13:00 \x1b[0m\n'
+        'To:   \x1b[34m04.10.2017\x1b[0m \x1b[34m18:00 \x1b[0m\n'
+    )
+
+    assert editor.startdt == BERLIN.localize(dt.datetime(2017, 10, 2, 13))
+    assert editor.enddt == BERLIN.localize(dt.datetime(2017, 10, 4, 18))
+    assert editor.changed is False
+    assert editor.allday is False
+
+    # set to all day event
+    editor.keypress((10, ), 'shift tab')
+    editor.keypress((10, ), ' ')
+    for _ in range(3):
+        editor.keypress((10, ), 'tab')
+    for _ in range(3):
+        editor.keypress((10, ), 'shift tab')
+
+    canvas = editor.render((50, ), True)
+    assert CanvasTranslator(canvas, palette).transform() == (
+        '[X] Allday\nFrom: \x1b[34m02.10.2017\x1b[0m  \n'
+        'To:   \x1b[34m04.10.2017\x1b[0m  \n'
+    )
+
+    assert editor.changed is True
+    assert editor.allday is True
+    assert editor.startdt == dt.date(2017, 10, 2)
+    assert editor.enddt == dt.date(2017, 10, 4)
