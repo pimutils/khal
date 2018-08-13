87a537747e5fcb654801002c2341fa3d4f9bdae9
Christian Geier <geier@lostpackets.de>
2018-08-13 23:16:02 +0200

    unpinning dateutil <= 2.6.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
     'urwid',
     'pyxdg',
     'pytz',
-    'python-dateutil<=2.6.1',
+    'python-dateutil',
     'configobj',
     # https://github.com/untitaker/python-atomicwrites/commit/4d12f23227b6a944ab1d99c507a69fdbc7c9ed6d  # noqa
     'atomicwrites>=0.1.7',
