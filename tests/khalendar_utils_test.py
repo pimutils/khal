aae086cd6c780ea13a3dc70dc1dbec540af9df48
Christian Geier <geier@lostpackets.de>
2021-03-13 21:13:13 +0100

    Make flake8 happy

--- tests/khalendar_utils_test.py
+++ tests/khalendar_utils_test.py
@@ -690,7 +690,7 @@ class TestSpecial(object):
         # TODO test for logging message
         assert dtstart is False
 
-    def test_event_dt_rrule_until_before_start(self):
+    def test_event_invalid_rrule(self):
         """test handling if an event with RRULE will never occur"""
         vevent = _get_vevent(_get_text('event_rrule_no_occurence'))
         dtstart = icalendar_helpers.expand(vevent, berlin)
